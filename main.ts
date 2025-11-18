fbc25a9adc6cd7f77f35a55c1a7b1ade5fbd769d
tipyman <149936594+tipyman@users.noreply.github.com>
2025-11-18 17:06:06 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,44 @@
-//% color=#0fbc11 icon="\uf2db" block="ZETag_R2"
+/**
+ * makecode ZETag module Package Release 2.
+ * By 2025 Socionext Inc. and ZETA alliance Japan
+ * Written by M.Urade　2025/11/18
+ */
+
+/**
+ * ZETag block
+ */
+
+//% weight=100 color=#0fbc11 icon="\uf482" block="ZETag_R2"
 namespace ZETag_R2 {
+    let l = 0
+    let i = 0
+    let Query_data: number[] = []
+    let sum = 0
+    let rx_data = 0
+
     /**
-     * UARTで数値をバイナリ送信します
-     * @param value 送信する数値
-     */
-    //% block="UARTでバイナリ送信 %value"
-    export function UART_BIN_TX(value: number): void {
+     * Binary data transmission over UART
+     * @param TX_data: 8bit data 
+    */
+    //% blockId=uart_tx_block="UART_BIN_TX %TX_data"
+    //% weight=80 blockGap=8
+    //% TX_data.min=0 TX_data.max=255 TX_data.defl=0
+    export function UART_BIN_TX(TX_data: number): void {
         dataBuffer.setUint8(0, TX_data);
         // バッファをシリアルポートに書き込む s
         serial.writeBuffer(dataBuffer)
     }
 
+    /**
+     * Binary data reception over UART
+     * @param value: none
+     * @return value: 16bit data When Upper 8 bit is 1, could not receive 
+    */
+    //% blockId=uart_rx_block="UART_BIN_RX"
+    //% weight=80 blockGap=8
+
     // 256回RX確認します。
     // データが取れれば00-FFのデータを返し、取れなければ0x100を返します。
-    /**
-     * UARTで数値をバイナリ受信します
-     * @param value 送信する数値
-     */
-    //% block="UARTでバイナリ受信"
     export function UART_BIN_RX(): number {
         l = 0
         while (l < 256) {
@@ -31,14 +52,21 @@ namespace ZETag_R2 {
     }
 
     /**
-     * UARTでZETagコマンドを受信します
-     * @param arr データ配列
-     * @param num1 数値1
-     * @param num2 数値2
-     */
-    //% block="ZETagコマンド実行    配列 %TX_array サイズ %TX_array_size クエリサイズ %Query_size"
-    export function ZETag_command (TX_array: number[], TX_array_size: number, Query_size: number): array {
-        Query_data = [0,0,0,0,0,0,0,0,0]
+     * ZETag command execution
+     * @param TX_array : number[]
+     * @param TX_array_size : number
+     * @param Query_size: numbe
+     * @return array[] 
+        array[0]: 0xff	Query data is ready
+              1	Timeout error
+              2	Size error (Query size <> Receipt size)
+              3	Checksum error
+    */
+    //% blockId=ZETag command execution block="ZETag command % TX_array %TX_array_size %Query_size"
+    //% weight=80 blockGap=8
+    //% Query_size.min=5 Query_size.max=9 Query_size.defl=5
+    export function ZETag_command(TX_array: number[], TX_array_size: number, Query_size: number): array {
+        Query_data = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         i = 0
         for (let index = 0; index < TX_array_size; index++) {
             UART_BIN_TX(TX_array[i])
@@ -78,12 +106,6 @@ namespace ZETag_R2 {
         return Query_data
     }
 
-let ZETag_buffer: Buffer = null
-let l = 0
-let i = 0
-let Query_data: number[] = []
-let sum = 0
-let rx_data = 0
-let buffer: Buffer = Buffer.create(0)
-let dataBuffer = pins.createBuffer(1);
+    let buffer: Buffer = Buffer.create(0)
+    let dataBuffer = pins.createBuffer(1);
 }
